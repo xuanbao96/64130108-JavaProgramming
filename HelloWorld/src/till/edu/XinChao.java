3dd31d2b595f2bfbe7ff225606f65bd1e95af5d8
xuanbao96 <baod7932@gmail.com>
2025-02-11 18:25:42 +0700

    Ví dụ đầu tiên
    Demo thử nghiệm

---
+++ HelloWorld/src/till/edu/XinChao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package till.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Tôi lần đầu học JAVA, và đang thử nghiệm quản lý mã nguồn với Git");
+	}
+
+}

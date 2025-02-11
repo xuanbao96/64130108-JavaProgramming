689dad5d063c99bd3faac2af7bba658e798a4a4f
xuanbao96 <baod7932@gmail.com>
2025-02-11 18:30:40 +0700

    Cập nhật code
    Sửa mới và thêm dòng

--- HelloWorld/src/till/edu/XinChao.java
+++ HelloWorld/src/till/edu/XinChao.java
@@ -13,7 +13,8 @@ public class XinChao {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Tôi lần đầu học JAVA, và đang thử nghiệm quản lý mã nguồn với Git");
+        System.out.println("Tôi lần đầu học Java, và đang thử nghiệm quản lý mã nguồn với Git");
+        System.out.print("Tôi thêm dòng mới và sửa lại chữ Java ở trên");
 	}
 
 }

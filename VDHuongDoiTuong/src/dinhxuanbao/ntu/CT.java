0ee10651412bb6bb469dc1158dbbda6731207ff6
xuanbao96 <baod7932@gmail.com>
2025-02-18 17:46:17 +0700

    Cập nhật code phần CT
    Dùng mảng để hiển thị sản phẩm

--- VDHuongDoiTuong/src/dinhxuanbao/ntu/CT.java
+++ VDHuongDoiTuong/src/dinhxuanbao/ntu/CT.java
@@ -1,22 +1,26 @@
 package dinhxuanbao.ntu;
+import java.util.ArrayList;
 
 public class CT {
 
 	public static void main(String[] args) {
-		// Tạo ra 2 sản phẩm
-		SanPham sp1 = new SanPham(0,"","","");
-		SanPham sp2 = new SanPham(2, "Bánh chuối", "Đồ ăn nhanh", "bc.jpg" );
-		sp1.setMaSP(1);
-		sp1.setTenSP("Bia SG");
-		sp1.setLoaiSP("Đồ uống có cồn");
-		sp1.setAnhSP("sg.png");
+		// Khai báo danh sách sản phẩm
+		ArrayList<SanPham> dsSanPham;
+		dsSanPham = new ArrayList<SanPham>();
 		
-		// In ra màn hình thông tin 2 sản phẩm
-		String thongtinSP1 = sp1.toString();
-		String thongtinSP2 = "Mã SP2 là" + sp2.getMaSP();
-		       thongtinSP2 = "Tên SP2 là" + sp2.getTenSP();
-		       
-		System.out.println(thongtinSP1);
-		System.out.println(thongtinSP2);
+		//Nhập 3 sản phẩm
+		SanPham sp1 = new SanPham(0, "Kem", "Giải khát", null);
+		SanPham sp2 = new SanPham(1, "Cơm", "Đồ ăn", null);
+		SanPham sp3 = new SanPham(2, "Phở", "Đồ ăn", null);
+		
+		//Thêm vào ArrayList
+		dsSanPham.add(sp1);
+		dsSanPham.add(sp2);
+		dsSanPham.add(sp3);
+		
+		for(int i=0; i<dsSanPham.size(); i++) {
+			SanPham sp = dsSanPham.get(i);
+			System.out.println(sp.toString());
+		}
 	}
 }
